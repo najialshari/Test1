6107a440c0e4fb525502b95082ab511d955817f5
najialshari <najigoon2@gmail.com>
2022-07-17 12:29:44 +0300

    Adding hover to Footer Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,9 +16,9 @@ const Navbar = () => {
         } else {
             setColorBlog('rgb(255, 255, 255)')
             setColorHome('rgb(204, 204, 204)')
+
         }
-        setOldHome('rgb(255, 255, 255)')
-        setOldBlog('rgb(255, 255, 255)')
+
     }
 
     const mouseEnter = (e) => {
@@ -30,6 +30,7 @@ const Navbar = () => {
             setColorBlog('rgb(0, 0, 255)')
         }
     }
+    
     const mouseLeave = (e) => {
         if (e.target.id === 'home') setColorHome(oldHome)
         else setColorBlog(oldBlog)
@@ -39,13 +40,13 @@ const Navbar = () => {
     return (
         <div className='navContainer'>
             {/* mainContainer used to wrap all components with lower layer z-index 1  */}
-            <nav className='navbar fixed-top bg-dark text-white p-0'>
+            <nav className='navbar fixed-top bg-dark p-0'>
                 <div className='container'>
                     <div className='col text-start py-1'>
                         <img src={myLogo} width="90px" height="60px" alt='...' />
                     </div>
-                    
-                    <div className='d-flex col-2 justify-content-end'>
+
+                    <div className='links d-flex col-2 justify-content-end'>
 
                         <Link id='home' to='/' className='nav-link p-2 me-3'
                             onMouseEnter={mouseEnter}
